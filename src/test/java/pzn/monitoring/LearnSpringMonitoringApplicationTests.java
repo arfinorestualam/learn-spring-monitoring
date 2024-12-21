6a95a77629ec80119edc179c6a96baacb3fca1fe
arfinorestualam <arfino.alam@gmail.com>
2024-12-21 21:20:24 +0700

    first

---
+++ src/test/java/pzn/monitoring/LearnSpringMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package pzn.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
