b61da80dd52f7b0de91a20438c35705d0d3c75bb
arfinorestualam <arfino.alam@gmail.com>
2024-12-28 19:00:04 +0700

    make custom metrics check

--- src/main/java/pzn/monitoring/MyScheduleTask.java
+++ src/main/java/pzn/monitoring/MyScheduleTask.java
@@ -1,6 +1,8 @@
 package pzn.monitoring;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -9,8 +11,15 @@ import org.springframework.stereotype.Component;
 //it is just test for actuator
 public class MyScheduleTask {
 
+    //add this for micrometer
+    @Autowired
+    private MeterRegistry meterRegistry;
+    //micrometer is library for using metrics, to make custom metrics, can use class above that use
+    //library micrometer. To see integrate using promotheus etc, check the documentation
+
     @Scheduled(fixedRate = 10000)
     public void hello() {
+        meterRegistry.counter("my.scheduled.task").increment();
         log.info("hello");
     }
 }
